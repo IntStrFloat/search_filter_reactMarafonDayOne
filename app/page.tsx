850fd0625744b927482040646460ce64202afeaf
IntStrFloat <108020874+IntStrFloat@users.noreply.github.com>
2023-08-27 19:48:27 +0300

    Filtering the name by the beginning

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,28 @@ import { useState } from "react";
 
 export default function Home ():React.ReactElement<any> {
   const [name,setName] = useState('');
-  console.log(names)
+  function check(first: string,second:string):boolean {
+    let flag = true;
+    let lenght = first.length < second.length ? first.length : second.length;
+    for( let i = 0; i < lenght; i++ ) {
+      if(first[i] !== second[i]) {
+        flag = false;
+      }
+    }
+    return flag;
+  }
   return (
     <div className={styles.container}>
       <div className={styles.app}>
-        <input placeholder="Начните вводить имя" type="text" />
+        <input onChange={(e)=>setName(e.target.value)}  placeholder="Начните вводить имя" type="text" />
         <ul>
-          {names.map((name)=><li>{name}</li>)}
+          {names.filter((el)=>{
+            if(name==='') {
+              return el
+            } else if (check(name,el)) {
+              return el
+            }
+          }).map((elem)=> {return <li>{elem}</li>})}
         </ul>
       </div>
     </div>
